809b15a4c76d268d3d9d139455b08a17ed12edd7
Ashish Sareen <ashish.sareen95@gmail.com>
2017-11-12 10:36:03 +0000

    Add new bank accs

--- railsBank/apis/urls.py
+++ railsBank/apis/urls.py
@@ -4,9 +4,11 @@ from . import views
 # apis
 urlpatterns = [
     url(r'^add_user$', views.add_user, name='add_user'),
+    url(r'^add_new_bank_account', views.add_new_bank_account, name='add_new_bank_account'),
     url(r'^add_bank_account$', views.add_bank_account, name='add_bank_account'),
     url(r'^get_bank_accounts$', views.get_bank_accounts, name='get_bank_accounts'),
     url(r'^get_user_details', views.get_user_details, name='get_user_details'),
+    # get total balance from all bank accounts
     url(r'^sign_in$', views.sign_in, name='sign_in'),
     url(r'^csrf_token$', views.get_csrf_token, name='csrf_token'),
 ]
--- railsBank/apis/views.py
+++ railsBank/apis/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import time
+import random
 import json
 import re
 from railsBank.settings import RAILSBANK_API_KEYS
 from apis.models import Users, BankAccounts
-
+from django.core import serializers
 # Create your views here.
 from django.http import HttpResponse, response
 from django.middleware.csrf import get_token
@@ -50,6 +52,26 @@ def _check_password(hashed_password, unhashed_user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + unhashed_user_password.encode()).hexdigest()
 
+
+def _rails_bank_post_request(body, url, is_json=False, is_get=False):
+    """
+    :param body: 
+    :param url: 
+    :param is_json: 
+    :return: 
+    """
+    # url = 'https://play.railsbank.com/v1/customer/endusers'
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Authorization': RAILSBANK_API_KEYS}
+
+    if is_get:
+        return requests.get(url=url, headers=headers)
+
+    if is_json:
+        return requests.post(url=url, json=body, headers=headers)
+    else:
+        return requests.post(url=url, data=body, headers=headers)
 
 EMAIL_REGEX = "[^@]+@[^@]+\.[^@]+"
 
@@ -171,6 +193,92 @@ def _get_user_from_user_id(enduser_id):
     return False, user
 
 
+def _random_alpha_gen(len=16):
+    return ''.join(random.choice('0123456789ABCDEF') for i in range(len))
+
+
+def _random_int_gen(len=6):
+    return ''.join(random.choice('0123456789') for i in range(len))
+
+
+@csrf_exempt
+def add_new_bank_account(request):
+    if request.method != "POST":
+        return HttpResponse("only POST operations permitted",
+                            content_type="text/plain",
+                            status=400)
+    try:
+        body = json.loads(request.body)
+    except Exception as e:
+        return _default400(e)
+
+    # inputs
+    enduser_id = body["enduser_id"]
+    preference = body["preference"]
+    account_name = body["account_name"]
+
+    val, resp = _get_user_from_user_id(enduser_id)
+    if val:
+        return resp
+    user = resp
+    req_data = {
+        "holder_id": enduser_id,
+        "partner_product": "ExampleBank-EUR-1",
+        "asset_class": "currency",
+        "asset_type": "eur",
+        "ledger_type": "ledger-type-single-user",
+        "ledger_who_owns_assets": "ledger-assets-owned-by-me",
+        "ledger_primary_use_types": ["ledger-primary-use-types-payments"],
+        "ledger_t_and_cs_country_of_jurisdiction": "GB"
+    }
+    url = "https://play.railsbank.com/v1/customer/ledgers"
+    responsed = _rails_bank_post_request(req_data, url, is_json=True)
+
+    if responsed.status_code != requests.codes.ok:
+        logger.error("Could not create ledger: {}".format(responsed.status_code))
+        return HttpResponse(resp, content_type="application/json", status=400)
+
+    logger.debug("response: type : {} , body: {}".format(type(responsed), responsed.json()))
+    ledger_id = responsed.json().get("ledger_id")
+
+    # post iban details
+    url = "https://play.railsbank.com/v1/customer/ledgers/{}/assign-iban".format(ledger_id)
+    logger.debug(url)
+
+    time.sleep(5)
+
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Authorization': RAILSBANK_API_KEYS}
+    responsed = requests.post(url=url, headers=headers)
+
+    if responsed.status_code != requests.codes.ok:
+        logger.error("Could not add iban: {}".format(responsed.status_code))
+        logger.error("Response: {}".format(responsed.content))
+        return HttpResponse(resp, content_type="application/json", status=400)
+
+    url = "https://play.railsbank.com/v1/customer/ledgers/{}".format(ledger_id)
+
+    logger.debug("Posting get request to fetch ledgers")
+
+    time.sleep(4)
+    responsed = _rails_bank_post_request('', url=url, is_get=True)
+    if responsed.status_code != requests.codes.ok:
+        logger.error("Could not fetch ledger: {}".format(responsed.status_code))
+        return HttpResponse(resp, content_type="application/json", status=400)
+
+    logger.debug("getting iban details : response: {}".format(responsed.json()))
+    iban = responsed.json().get("iban")
+    swift_code = responsed.json().get("bic_swift", 400334)
+
+    user.bankaccounts_set.create(ledger_id=ledger_id,
+                                 preference=preference,
+                                 iban=iban,
+                                 swift_code=swift_code,
+                                 account_name=account_name)
+    return HttpResponse(resp, status=200, content_type="application/json")
+
+
 # adds only the existing ledgers and iBans , does not create new ones.
 @csrf_exempt
 def add_bank_account(request):
@@ -185,18 +293,24 @@ def add_bank_account(request):
 
     enduser_id = body["enduser_id"]
     preference = body["preference"]
+    ledger_id = body["ledger_id"]
+    iban = body["iban"]
+    swift_code = body["swift_code"]
+    account_name = body["account_name"]
+
     val, resp = _get_user_from_user_id(enduser_id)
     if val:
         return resp
     user = resp
 
-    if len(user.bankaccounts_set.all()) == 0:
-        global preference
-        preference = 1
-    else:
-        # change the preference of all bank accounts
-        for i in user.bankaccounts_set.all():
-            pass
+    user.bankaccounts_set.create(ledger_id=ledger_id,
+                                 preference=preference,
+                                 iban=iban,
+                                 swift_code=swift_code,
+                                 account_name=account_name)
+
+    return HttpResponse(status=200, content_type="application/json")
+
 
 @csrf_exempt
 def get_user_details(request):
@@ -214,14 +328,17 @@ def get_user_details(request):
     if val:
         return resp
     user = resp
+    user_data = serializers.serialize('json', user.bankaccounts_set.all())
     response = {
         "username": user.username,
         "enduser_id": user.enduser_id,
         "email": user.email,
-        "totalSavings": user.totalSavings
+        "totalSavings": user.totalSavings,
+        "bank_accounts": user_data
     }
-    response = json.dumps(response)
-    return HttpResponse(response, content_type="application/json", status=200)
+    return HttpResponse(json.dumps(response),
+                        content_type="application/json",
+                        status=200)
 
 
 @csrf_exempt
@@ -241,14 +358,14 @@ def get_bank_accounts(request):
         return resp
     user = resp
     resp_list = []
-    for bankaccs in user.bankaccounts_set.all():
+    for bank_accs in user.bankaccounts_set.all():
         acc = {}
         acc["user"] = enduser_id
-        acc["ledger_id"] = bankaccs.ledger_id
-        acc["preference"] = bankaccs.preference
-        acc["iban"] = bankaccs.iban
-        acc["swift_code"] = bankaccs.swift_code
-        acc["account_name"] = bankaccs.account_name
+        acc["ledger_id"] = bank_accs.ledger_id
+        acc["preference"] = bank_accs.preference
+        acc["iban"] = bank_accs.iban
+        acc["swift_code"] = bank_accs.swift_code
+        acc["account_name"] = bank_accs.account_name
         resp_list.append(acc)
     rsp = json.dumps({"accounts": resp_list})
     logger.info(rsp)
